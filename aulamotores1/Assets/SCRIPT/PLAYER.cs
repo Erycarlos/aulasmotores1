30821c55e6256c0ec9d33e567a490ac415ba1d6e
Erycarlos <geraldo.erycarlos@escolar.ifrn.edu.br>
2025-05-05 12:02:05 -0300

    pokemon3

---
+++ aulamotores1/Assets/SCRIPT/PLAYER.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PLAYER : MonoBehaviour
+{
+    private int numero;
+    public float velocidade; = 40;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
